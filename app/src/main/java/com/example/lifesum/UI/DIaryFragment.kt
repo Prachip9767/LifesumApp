05123bcfde2fec66a076bb165c62fe20e327ff8b
Prachip9767 <prachip293@gmail.com>
2021-09-29 11:46:15 +0530

    Prachi

---
+++ app/src/main/java/com/example/lifesum/UI/DIaryFragment.kt
@@ -0,0 +1,13 @@
+package com.example.lifesum.UI
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lifesum.R
+
+
+class DIaryFragment : Fragment(R.layout.fragment_d_iary) {
+
+}

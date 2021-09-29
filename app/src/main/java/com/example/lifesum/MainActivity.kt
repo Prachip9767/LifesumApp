899fe1076bf9e5a14d8da182bcefb0e4541130be
Prachip9767 <prachip293@gmail.com>
2021-09-29 21:25:49 +0530

    Prachi

--- app/src/main/java/com/example/lifesum/MainActivity.kt
+++ app/src/main/java/com/example/lifesum/MainActivity.kt
@@ -7,7 +7,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_d_iary)
+        Water.setOnClickListener {
+            Water.playAnimation()
+        }
+        Water2.setOnClickListener {
+            Water2.playAnimation()
+        }
+        Water3.setOnClickListener {
+            Water3.playAnimation()
+        }
+        Water4.setOnClickListener {
+            Water4.playAnimation()
+        }
     }
+
 }
 //Prachi Pardeshi

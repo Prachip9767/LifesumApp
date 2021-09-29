d15520d832f7ac65722f8f865ea222b36544f4d8
Prachip9767 <prachip293@gmail.com>
2021-09-29 18:26:30 +0530

    Prachi

--- app/src/main/java/com/example/lifesum/UI/DIaryFragment.kt
+++ app/src/main/java/com/example/lifesum/UI/DIaryFragment.kt
@@ -6,8 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lifesum.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class DIaryFragment : Fragment(R.layout.fragment_d_iary) {
-
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Water.setOnClickListener {
+            Water.playAnimation()
+        }
+        Water2.setOnClickListener {
+            Water2.playAnimation()
+        }
+        Water3.setOnClickListener {
+            Water3.playAnimation()
+        }
+        Water4.setOnClickListener {
+            Water4.playAnimation()
+        }
+    }
+    }
